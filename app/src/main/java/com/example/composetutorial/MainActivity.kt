9df2c8dabe3a74be44174536e1f10993f59a3c04
Uglus <ugluss@gmail.com>
2025-08-05 13:15:24 +0300

    Lesson 3 finished

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,13 +25,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message(author = "Stepan", body = "Hello World"))
+            Surface(modifier = Modifier.fillMaxSize()) {
+                MessageCard(Message(author = "Stepan", body = "Hello World"))
+            }
+
         }
     }
 
@@ -33,36 +43,62 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(message: Message){
-        Row(modifier = Modifier.padding(all = 20.dp)) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
 
             Image(
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = "Contact profile picture",
                 modifier = Modifier
-                    .size(40.dp)
+                    .size(50.dp)
                     .clip(RoundedCornerShape(50))
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
-                Text(text = "Author of message: ${message.author}")
+            Column() {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleSmall
+                    )
+
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = "Text of message: ${message.body}")
+
+                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                    Row(modifier = Modifier.padding(all = 4.dp)){
+                        Text(text = "Text: ",
+                            color = MaterialTheme.colorScheme.secondary,
+                            style =  MaterialTheme.typography.bodyMedium
+                        )
+                        Text(
+                            text = message.body,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        name = "Dark mode",
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true
+    )
+    @Composable
+    fun PreviewMessageCard(){
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(
+                    message = Message(author = "Stepan", body = "Hello World")
+                )
             }
         }
 
-
-
-
-    }
-
-    @Preview
-    @Composable
-    fun PreviewMessageCard(){
-        MessageCard(
-            message = Message(author = "Stepan", body = "Hello World")
-        )
     }
 
 }
