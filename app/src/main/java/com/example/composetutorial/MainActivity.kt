d89a7fbd39c7e0321a269b3eeb2e5c67263878b6
Uglus <ugluss@gmail.com>
2025-08-05 15:51:05 +0300

    Lesson 4, Lists and animations finished

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +17,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +31,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 class MainActivity : ComponentActivity() {
@@ -33,13 +42,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(modifier = Modifier.fillMaxSize()) {
-                MessageCard(Message(author = "Stepan", body = "Hello World"))
+                Conversation(SampleData.conversationSample)
             }
 
         }
     }
 
-    data class Message (val author: String, val body:String)
+
+
+    @Composable
+    fun Conversation(messages: List<Message>){
+        LazyColumn {
+            items(messages){message->
+                MessageCard(message)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation(){
+        ComposeTutorialTheme {
+            Conversation(SampleData.conversationSample)
+        }
+    }
 
     @Composable
     fun MessageCard(message: Message){
@@ -56,7 +82,13 @@ class MainActivity : ComponentActivity() {
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column() {
+
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+            )
+
+            Column(modifier = Modifier.clickable{isExpanded = !isExpanded}) {
                 Text(
                     text = message.author,
                     color = MaterialTheme.colorScheme.primary,
@@ -65,7 +97,11 @@ class MainActivity : ComponentActivity() {
 
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)) {
                     Row(modifier = Modifier.padding(all = 4.dp)){
                         Text(text = "Text: ",
                             color = MaterialTheme.colorScheme.secondary,
@@ -73,6 +109,7 @@ class MainActivity : ComponentActivity() {
                         )
                         Text(
                             text = message.body,
+                            maxLines = if(isExpanded) Int.MAX_VALUE else 1,
                             style = MaterialTheme.typography.bodyMedium
                         )
                     }
